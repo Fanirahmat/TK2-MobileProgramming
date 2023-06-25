dfb6a0629f3f1093f3c8e20de1241873a7003e53
Mohammad Fani Rahmatulloh <q@Fanis-MacBook-Pro.local>
2023-06-25 14:14:05 +0700

    [FINISH] Home page

--- app/src/main/java/com/example/tk2/MainActivity.kt
+++ app/src/main/java/com/example/tk2/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tk2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnNext = findViewById<Button>(R.id.button_next)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
